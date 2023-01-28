0bfbd680732fe97b14118f171b5e3f5b031b869e
zhanat güneş <guneszhanat@gmail.com>
2023-01-28 18:51:02 +0300

    excelstepdefinitions

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,14 +1,18 @@
 package pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(id="dropdown-basic-button")
     public WebElement userID;
+    @FindBy(partialLinkText = "Login")
+    public WebElement homePageLoginLink;
+    @FindBy(linkText = "Logout")
+    public WebElement logoutLink;
+    @FindBy(xpath = "//button[text()='OK']")
+    public WebElement OK;
 }
--- src/test/java/stepdefinitions/ExcelStepDefinitions.java
+++ src/test/java/stepdefinitions/ExcelStepDefinitions.java
@@ -1,18 +1,73 @@
 package stepdefinitions;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-
+import org.junit.Assert;
+import pages.HomePage;
+import pages.LoginPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ExcelUtil;
+import utilities.ReusableMethods;
+import java.util.List;
+import java.util.Map;
 public class ExcelStepDefinitions {
-
-
+    HomePage homePage;
+    LoginPage loginPage;
+    ExcelUtil excelUtil;
+    //    will store excel data
+    List<Map<String,String>> allTestData;
+    //    1. Create a login method
+    public void login(){
+        Driver.getDriver().get(ConfigReader.getProperty("app_home_url"));
+        homePage = new HomePage();
+        loginPage =new LoginPage();
+        ReusableMethods.waitFor(1);
+        try {
+            homePage.homePageLoginLink.click();
+            ReusableMethods.waitFor(1);
+        }catch (Exception e){
+        }
+        try{
+            ReusableMethods.waitFor(1);
+            homePage.userID.click();
+            ReusableMethods.waitFor(1);
+            homePage.logoutLink.click();
+            ReusableMethods.waitFor(1);
+            homePage.OK.click();
+            ReusableMethods.waitFor(1);
+            homePage.homePageLoginLink.click();
+            ReusableMethods.waitFor(1);
+        }catch (Exception e){
+        }
+        ReusableMethods.waitFor(1);
+    }
     @Given("user tries to login as {string}")
     public void user_tries_to_login_as(String string) {
-
-
+        String path = "./src/test/java/resources/mysmoketestdata.xlsx";
+        String sheetName = "customer_info";
+        excelUtil = new ExcelUtil(path, sheetName);
+//        getDataList() method returns all excel data
+//        we stored that data in allTestData variable
+        allTestData = excelUtil.getDataList();
+//        System.out.println(allTestData);
+//        System.out.println(excelUtils.getColumnsNames());
+//        System.out.println(excelUtils.getCellData(2,1));
+//        USING LOOP GET THE VALUES FROM THE MAP AND SEND IN THE UI
+        for (Map<String, String> eachData : allTestData) {
+//            Takes us to the login page
+            login();
+            loginPage.username.sendKeys(eachData.get("username"));
+            ReusableMethods.waitFor(1);
+            loginPage.password.sendKeys(eachData.get("password"));
+            ReusableMethods.waitFor(1);
+            loginPage.loginButton.click();
+            ReusableMethods.waitFor(1);
+//            Verify if login is successful
+//            IF USER ID IS DISPLAYED THEN LOGIN IS SUCCESSFUL
+            Assert.assertTrue(homePage.userID.isDisplayed());
+        }
     }
     @Then("capture the screenshot")
     public void capture_the_screenshot() {
-
     }
 }
