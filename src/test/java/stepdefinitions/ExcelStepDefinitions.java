a144c9981867741a1f958a56b39c104e2d48a4cd
zhanat güneş <guneszhanat@gmail.com>
2023-01-28 18:41:29 +0300

    excelutil

---
+++ src/test/java/stepdefinitions/ExcelStepDefinitions.java
@@ -0,0 +1,18 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class ExcelStepDefinitions {
+
+
+    @Given("user tries to login as {string}")
+    public void user_tries_to_login_as(String string) {
+
+
+    }
+    @Then("capture the screenshot")
+    public void capture_the_screenshot() {
+
+    }
+}
